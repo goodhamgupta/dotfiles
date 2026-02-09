3b74cebe551157828a96a15b90118c6dc22655cd
Shubham Gupta <shubhamg2208@live.com>
2026-02-09 16:46:19 +0800

    Add catppuccin theme

---
+++ nvim/lua/plugins/catppuccin.lua
@@ -0,0 +1,27 @@
+---@type LazySpec
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha",
+      term_colors = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        treesitter = true,
+        telescope = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+      },
+    },
+  },
+}
